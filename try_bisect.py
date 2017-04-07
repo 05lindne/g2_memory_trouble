14dfec02996dc5303cb0fa1c50ab1121cbf09575
Sahara <sahara@Sarahs-MacBook-Pro.local>
2017-04-07 18:09:10 +0900

    lookup, listsplittin, bin implemented for offset=0, scale=1, BIN BUGGY?

--- try_bisect.py
+++ try_bisect.py
@@ -5,19 +5,29 @@ import matplotlib.pyplot as plt
 from bisect import bisect_left
 
 
-df1 = pd.Series([2,10,11,12,14,20,21,22], dtype=np.float64)
-df2 = pd.Series([3,4,5,8,12,14,16,23,24], dtype=np.float64)
+df1 = pd.Series([5,10,11,12,14,20,21,22,26], dtype=np.float64)
+df2 = pd.Series([3,4,6,8,12,14,16,23,24,25], dtype=np.float64)
 
 value = 5
 
 print "location of inserting ", value, " in dataframe df2: ", bisect_left(df2, 5)
 
+
 for item in df1:
+
+	print "old df2: "
+	print df2
 
 	insertion_point = bisect_left(df2, item)
 
-	print item 
-	print "inserted at: ", insertion_point
-	print df2
+	print "value ", item, " inserted at: ", insertion_point
+
 	# cut away data lower than insertion point
-	# df2 = df2[insertion_point:]
+	df2 =  df2[insertion_point:]
+	df2 = df2.reset_index(drop=True)
+	bin = df2 - item
+
+	print "bins: "
+	print bin
+	print "new df2: "
+	print df2
