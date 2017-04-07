fd3648c231ed1efebb54660436b981efcd9f30b1
Sahara <sahara@Sarahs-MacBook-Pro.local>
2017-04-07 17:31:03 +0900

    try to find position with bisect

--- try_bisect.py
+++ try_bisect.py
@@ -10,9 +10,14 @@ df2 = pd.Series([3,4,5,8,12,14,16,23,24], dtype=np.float64)
 
 value = 5
 
-print "location of inserting ", value, " in dataframe df1: ", bisect_left(df1, 5)
+print "location of inserting ", value, " in dataframe df2: ", bisect_left(df2, 5)
 
-for item in df2:
+for item in df1:
+
+	insertion_point = bisect_left(df2, item)
 
 	print item 
-	print "inserted at: ", bisect_left(df1, item)
+	print "inserted at: ", insertion_point
+	print df2
+	# cut away data lower than insertion point
+	# df2 = df2[insertion_point:]
